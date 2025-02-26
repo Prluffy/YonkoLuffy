a83bbbdebde323fb5a60ade505cf7f873708737e
Admin <Admin@DESKTOP-UQJIA5A.mshome.net>
2025-02-26 14:57:43 +0530

    comitted

--- ERP_Dollibarr/src/main/java/com/Dollibarr/generic/ObjectRepositoryUtility/ProductsPage.java
+++ ERP_Dollibarr/src/main/java/com/Dollibarr/generic/ObjectRepositoryUtility/ProductsPage.java
@@ -1,9 +1,19 @@
 package com.Dollibarr.generic.ObjectRepositoryUtility;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class ProductsPage {
-		
-	//code 1
+	 //code 1
 	//code 2
 	//code 3
-	@findby
+	//@findby
+	
+//	WebDriver driver;
+	public static void main(String[] args) {
+	WebDriver driver= new ChromeDriver();
+	driver.get("http://www.amazon.com/");
+	driver.findElement(By.xpath("//label[text()='Search Amazon.in']")).click();
+	}
 }
