aa91da37a7d6eabf10576ac5d49691d4b921e5aa
lenovo <lenovo@Abhishek.mshome.net>
2025-02-26 15:37:18 +0530

    saved the file

--- ERP_Dollibarr/src/main/java/com/Dollibarr/generic/ObjectRepositoryUtility/ProductsPage.java
+++ ERP_Dollibarr/src/main/java/com/Dollibarr/generic/ObjectRepositoryUtility/ProductsPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 
 public class ProductsPage {
@@ -18,7 +19,7 @@ public class ProductsPage {
 	WebDriver driver= new ChromeDriver();
 	driver.get("http://www.amazon.com/");
 	driver.findElement(By.name("Search Amazon.in")).click();
-	ele=driver.findElement(By.id("apple 15 pro"));
+	WebElement ele = driver.findElement(By.id("apple 15 pro"));
 	Actions act= new Actions(driver);
 		act.moveToElement(ele);
 	}
