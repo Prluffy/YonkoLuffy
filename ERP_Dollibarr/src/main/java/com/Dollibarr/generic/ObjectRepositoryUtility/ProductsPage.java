3a8623ca2810f345cbfeae4c192214a6ba3305cf
lenovo <lenovo@Abhishek.mshome.net>
2025-02-26 15:02:03 +0530

    hi

--- ERP_Dollibarr/src/main/java/com/Dollibarr/generic/ObjectRepositoryUtility/ProductsPage.java
+++ ERP_Dollibarr/src/main/java/com/Dollibarr/generic/ObjectRepositoryUtility/ProductsPage.java
@@ -1,9 +1,13 @@
 package com.Dollibarr.generic.ObjectRepositoryUtility;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class ProductsPage {
 		
 	//code 1
 	//code 2
 	//code 3
-	@findby
+	@FindBy(name="luffy")
+	private WebElement luffy;
 }
