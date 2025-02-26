9f79e6ba9d1fd7ae781abf125b08398247cfc21b
lenovo <lenovo@Abhishek.mshome.net>
2025-02-26 16:19:12 +0530

    update

--- ERP_Dollibarr/src/main/java/com/Dollibarr/generic/ObjectRepositoryUtility/ProductsPage.java
+++ ERP_Dollibarr/src/main/java/com/Dollibarr/generic/ObjectRepositoryUtility/ProductsPage.java
@@ -1,6 +1,5 @@
 package com.Dollibarr.generic.ObjectRepositoryUtility;
 
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.By;
@@ -8,19 +7,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-
 public class ProductsPage {
-	 //code 1
-	//code 2
-	//code 3
-	@FindBy(name="luffy")
+	// code 1
+	// code 2
+	// code 3
+	@FindBy(name = "luffy")
 	private WebElement luffy;
+
 	public static void main(String[] args) {
-	WebDriver driver= new ChromeDriver();
-	driver.get("http://www.amazon.com/");
-	driver.findElement(By.name("Search Amazon.in")).click();
-	WebElement ele = driver.findElement(By.id("apple 15 pro"));
-	Actions act= new Actions(driver);
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.amazon.com/");
+		driver.findElement(By.name("Search Amazon.in")).click();
+		WebElement ele = driver.findElement(By.id("apple 15 pro"));
+		Actions act = new Actions(driver);
 		act.moveToElement(ele);
 	}
 }
